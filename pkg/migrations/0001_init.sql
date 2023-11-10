55f010e3618501ccec23e6bc352d79806dd691a7
k-orolevsk-y <i@korolevsky.me>
2023-11-10 16:44:44 +0300

    refactoring: database struct (accrual & balance int -> float64)

--- pkg/migrations/0001_init.sql
+++ pkg/migrations/0001_init.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    login varchar(100) NOT NULL,
+    login varchar(100) NOT NULL UNIQUE,
     password text NOT NULL,
-    balance int NOT NULL,
+    balance DOUBLE PRECISION NOT NULL,
     created_at timestamptz NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_users_login ON users (login);
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS orders (
     id bigint PRIMARY KEY  NOT NULL UNIQUE,
     user_id uuid NOT NULL,
     status varchar(28) NOT NULL DEFAULT 'NEW',
-    accrual int NOT NULL DEFAULT 0,
+    accrual DOUBLE PRECISION NOT NULL DEFAULT 0,
     uploaded_at timestamptz NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_orders_userId ON orders (user_id);
