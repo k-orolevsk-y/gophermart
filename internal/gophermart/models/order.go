7a4b437c3aaa1e1de069feb5a0a0bed9d7d18a5d
k-orolevsk-y <i@korolevsky.me>
2023-11-12 23:38:41 +0300

    feat: add json annotations to the order model

--- internal/gophermart/models/order.go
+++ internal/gophermart/models/order.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Order struct {
-	ID         int64     `db:"id"`
-	UserID     uuid.UUID `db:"user_id"`
-	Status     string    `db:"status"`
-	Accrual    float64   `db:"accrual"`
-	UploadedAt time.Time `db:"uploaded_at"`
+	ID         int64     `db:"id" json:"number,string"`
+	UserID     uuid.UUID `db:"user_id" json:"-"`
+	Status     string    `db:"status" json:"status"`
+	Accrual    *float64  `db:"accrual" json:"accrual,omitempty"`
+	UploadedAt time.Time `db:"uploaded_at" json:"uploaded_at"`
 }
 
 func (o Order) CheckValidID() bool {
