00a3f426e9a6ef096db97ad4906532b2d02c66d5
k-orolevsk-y <i@korolevsky.me>
2023-11-13 12:08:44 +0300

    refactoring: moved the function from the model to the service

--- internal/gophermart/models/order.go
+++ internal/gophermart/models/order.go
@@ -13,30 +13,3 @@ type Order struct {
 	Accrual    *float64  `db:"accrual" json:"accrual,omitempty"`
 	UploadedAt time.Time `db:"uploaded_at" json:"uploaded_at"`
 }
-
-func (o Order) CheckValidID() bool {
-	if o.ID == 0 {
-		return false
-	}
-
-	checkSum := func(number int64) int64 {
-		var controlSum int64
-
-		for i := 0; number > 0; i++ {
-			cur := number % 10
-
-			if i%2 == 0 {
-				cur *= 2
-				if cur > 9 {
-					cur = cur%10 + cur/10
-				}
-			}
-
-			controlSum += cur
-			number /= 10
-		}
-		return controlSum % 10
-	}
-
-	return (o.ID%10+checkSum(o.ID/10))%10 == 0
-}
