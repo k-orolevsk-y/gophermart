5cb095debf9a1d3cc935a8e43cb973379d8795e2
k-orolevsk-y <i@korolevsky.me>
2023-11-13 13:24:08 +0300

    feat: add withdrawals methods

--- internal/gophermart/models/user_withdraw.go
+++ internal/gophermart/models/user_withdraw.go
@@ -7,9 +7,9 @@ import (
 )
 
 type UserWithdraw struct {
-	ID          uuid.UUID `db:"id"`
-	UserID      uuid.UUID `db:"user_id"`
-	OrderID     int64     `db:"order_id"`
-	Sum         float64   `db:"sum"`
-	ProcessedAt time.Time `db:"processed_at"`
+	ID          uuid.UUID `db:"id" json:"-"`
+	UserID      uuid.UUID `db:"user_id" json:"-"`
+	OrderID     int64     `db:"order_id" json:"order,string"`
+	Sum         float64   `db:"sum" json:"sum"`
+	ProcessedAt time.Time `db:"processed_at" json:"processed_at"`
 }
