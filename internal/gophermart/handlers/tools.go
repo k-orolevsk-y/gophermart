dc9c0f5478ccf270f1e9d3d6e8d6c7d9268942f4
k-orolevsk-y <i@korolevsky.me>
2023-11-13 12:08:44 +0300

    refactoring: moved the function from the model to the service

--- internal/gophermart/handlers/tools.go
+++ internal/gophermart/handlers/tools.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"errors"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/k-orolevsk-y/gophermart/pkg/jwt"
@@ -18,4 +21,53 @@ func (hs *handlerService) GetTokenClaims(ctx *gin.Context) (*jwt.Claims, bool) {
 	}
 
 	return tokenClaims, true
+}
+
+var (
+	errInvalidNumberForAlgorithmLuna       = errors.New("invalid number")
+	errInvalidTypeOfNumberForAlogirthmLuna = errors.New("invalid type")
+)
+
+func (hs *handlerService) CheckNumberAlgorithmLuna(n any) (int64, error) {
+	var number int64
+	switch num := n.(type) {
+	case int64:
+		number = num
+	case string:
+		var err error
+		number, err = strconv.ParseInt(num, 10, 64)
+
+		if err != nil {
+			return 0, errInvalidTypeOfNumberForAlogirthmLuna
+		}
+	}
+
+	if number == 0 {
+		return 0, errInvalidNumberForAlgorithmLuna
+	}
+
+	checkSum := func(number int64) int64 {
+		var controlSum int64
+
+		for i := 0; number > 0; i++ {
+			cur := number % 10
+
+			if i%2 == 0 {
+				cur *= 2
+				if cur > 9 {
+					cur = cur%10 + cur/10
+				}
+			}
+
+			controlSum += cur
+			number /= 10
+		}
+		return controlSum % 10
+	}
+
+	if (number%10+checkSum(number/10))%10 == 0 {
+		return number, nil
+	}
+
+	return 0, errInvalidNumberForAlgorithmLuna
 }
