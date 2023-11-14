33545b59955eb312617f61a91ea56bcfe2cf480e
k-orolevsk-y <i@korolevsky.me>
2023-11-14 11:48:22 +0300

    feat: add recovery handler

--- internal/gophermart/handlers/errors.go
+++ internal/gophermart/handlers/errors.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+
+	"github.com/k-orolevsk-y/gophermart/internal/gophermart/models"
 )
 
 func (hs *handlerService) NoRoute(ctx *gin.Context) {
@@ -12,4 +15,9 @@ func (hs *handlerService) NoRoute(ctx *gin.Context) {
 
 func (hs *handlerService) NoMethod(ctx *gin.Context) {
 	ctx.AbortWithStatus(http.StatusMethodNotAllowed)
+}
+
+func (hs *handlerService) Recovery(ctx *gin.Context, err any) {
+	hs.logger.Error("Panic on request", zap.Any("error", err))
+	ctx.AbortWithStatusJSON(http.StatusInternalServerError, models.NewInternalServerErrorResponse())
 }
