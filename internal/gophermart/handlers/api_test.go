cb8e91bb5ce8d1acb3590731508e2106061f4636
k-orolevsk-y <i@korolevsky.me>
2023-11-15 20:43:09 +0300

    test(api): refactoring

--- internal/gophermart/handlers/api_test.go
+++ internal/gophermart/handlers/api_test.go
@@ -1,12 +1,9 @@
 package handlers
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/require"
 
 	"github.com/k-orolevsk-y/gophermart/internal/gophermart/config"
 	"github.com/k-orolevsk-y/gophermart/internal/gophermart/middlewares"
@@ -16,25 +13,12 @@ import (
 )
 
 func NewTestAPI(t *testing.T) *mocks.TestAPI {
-	if config.Config.HmacTokenSecret == "" {
-		GenerateHmacTokenSecret(t)
-	}
-
 	api := mocks.NewTestAPI(t)
 
 	ConfigureHandlersService(api)
 	middlewares.ConfigureMiddlewaresService(api)
 
 	return api
-}
-
-func GenerateHmacTokenSecret(t *testing.T) {
-	secret := make([]byte, 16)
-
-	_, err := rand.Read(secret)
-	require.NoError(t, err, "Не удалось сгенерировать secret-ключ для JWT")
-
-	config.Config.HmacTokenSecret = hex.EncodeToString(secret)
 }
 
 func GetUserIDWithToken() (string, uuid.UUID, error) {
