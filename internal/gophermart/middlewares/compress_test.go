0c54b5524b35265a3d025f605f72ee5ab1681db4
k-orolevsk-y <i@korolevsky.me>
2023-11-16 09:20:48 +0300

    test(compressMiddlewares): add compress middleware test

---
+++ internal/gophermart/middlewares/compress_test.go
@@ -0,0 +1,36 @@
+package middlewares
+
+import (
+	"compress/gzip"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMiddlewareCompress(t *testing.T) {
+	api := NewTestAPI(t)
+
+	w := httptest.NewRecorder()
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set("Accept-Encoding", "gzip")
+
+	api.GetRouter().ServeHTTP(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	contentEncoding := res.Header.Get("Content-Encoding")
+	_, err := gzip.NewReader(res.Body)
+
+	reqSuccess := assert.Contains(t, contentEncoding, "gzip", "Отсутствует header о сжатии через gzip")
+	reqSuccess = reqSuccess && assert.NoError(t, err, "Не удалось расшифровать тело ответа сжатое через gzip")
+
+	if !reqSuccess {
+		dump, _ := httputil.DumpRequest(req, true)
+		t.Logf("\nЗапрос:\n%s", dump)
+	}
+}
